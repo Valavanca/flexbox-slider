9fc75e493f05a60347dab78241d8cb359f5de3e6
Olexander Husak <valavanca@gmail.com>
2017-06-17 18:12:08 +0300

    remove jquery, optimize JS code

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-(function() {
-  var carousel = $('.carousel');
-  var seats = $('.carousel-seat'); // list of slider's items 
+  let carousel = document.getElementById('carousel')
+  let seats = document.getElementsByClassName("carousel-seat"); // list of slider's items 
 
   [].forEach.call(seats, function(element, index) { // default order
      element.style.order = index;
   });  
 
-  $('.toggle').on('click', function(e) {
-    var droppWhen = 0,                          // default moving back 
-    valueForDropp = seats.length-1,      //
-    delta = -1;                                         //
 
-    if ($(e.currentTarget).data('toggle') === 'next') {
-          droppWhen = seats.length-1;
-          valueForDropp = 0;
-          delta = 1;
-    }
+  [].forEach.call(document.getElementsByClassName("toggle"), function(el) {
+    el.addEventListener("click", function(e) {
+      let droppWhen = 0,                            // default config for moving back  
+      valueForDropp = seats.length-1,       //
+      delta = -1,                                         //
+      directionClass = "left";                      //
 
-    [].forEach.call(seats, function(element, index) { // shuffle cards
-      var tempOrder = parseInt(element.style.order, 10)
-      if (tempOrder===droppWhen) {
-        element.style.order = valueForDropp;
-      } else {
-        element.style.order = tempOrder + delta;
+      if (e.target.dataset.toggle === 'next') {
+            droppWhen = seats.length-1;
+            valueForDropp = 0;
+            delta = 1;
+            directionClass = "right"; 
       }
-    });        
 
-    carousel.removeClass('is-set');
-    return setTimeout((function() {
-      return carousel.addClass('is-set');
-    }), 50);
+      [].forEach.call(seats, function(element, index) { // shuffle cards
+        var tempOrder = parseInt(element.style.order, 10)
+        if (tempOrder===droppWhen) {
+          element.style.order = valueForDropp;
+        } else {
+          element.style.order = tempOrder + delta;
+        }
+      });        
+
+      carousel.className = "";
+      setTimeout((function() {
+        carousel.classList.add(directionClass);
+      }), 50);       
+    });
   });
-
-}).call(this);
