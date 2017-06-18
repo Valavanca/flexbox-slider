11c1d5319d827c2f4f3317f90210435de1253ecd
Olexander Husak <valavanca@gmail.com>
2017-06-18 21:18:46 +0300

    variant with shuffling class "hide"

--- src/index.js
+++ src/index.js
@@ -6,32 +6,59 @@
   });  
 
 
-  [].forEach.call(document.getElementsByClassName("toggle"), function(el) {
-    el.addEventListener("click", function(e) {
-      let droppWhen = 0,                            // default config for moving back  
-      valueForDropp = seats.length-1,       //
-      delta = -1,                                         //
-      directionClass = "left";                      //
+function displayWindow(colection) {
+     this.itemsCollection = colection;
+     this.length = colection.length;
+     this.left = 0;
+     this.right = 2;
+}
 
-      if (e.target.dataset.toggle === 'next') {
-            droppWhen = seats.length-1;
-            valueForDropp = 0;
-            delta = 1;
-            directionClass = "right"; 
+displayWindow.prototype.hideItems = function() {
+    function findeShowRange(index, left, right) {
+      let range; 
+      if(right>=left) {
+        range = index >= left && index <= right;
+      } else {
+        range = index >= left || index <= right
       }
+      return range;
+    } 
+    function changeOrder(element, index, context) {
+      if(context.left<context.right) {
+        element.style.order = index;
+      } else {
+        element.style.order = index < context.left ? context.length+index : index;
+      }
+    }
 
-      [].forEach.call(seats, function(element, index) { // shuffle cards
-        var tempOrder = parseInt(element.style.order, 10)
-        if (tempOrder===droppWhen) {
-          element.style.order = valueForDropp;
-        } else {
-          element.style.order = tempOrder + delta;
-        }
-      });        
+    [].forEach.call(this.itemsCollection, (element, index)=> {
+      if(findeShowRange(index, this.left, this.right)) {
+        changeOrder(element, index, this);
+        element.classList.remove("hide");
+        setTimeout((()=> {
+          element.classList.remove("opacity");
+        }), 1000);
+      } else {
+        element.classList.add("opacity");
+        setTimeout((()=> {
+          element.classList.add("hide");
+        }), 1000);
+      }
+    });  
+  };
+displayWindow.prototype.moveLeft = function() {
+/*    this.itemsCollection.item(this.right).classList.add("push_to");*/
+    this.left = (this.left===0) ? this.length-1 : this.left-1;
+    this.right = (this.right===0) ? this.length-1 : this.right-1;
+    this.hideItems();
+}
+displayWindow.prototype.moveRight = function() {
+    /* this.itemsCollection.item(this.left).classList.add("pull_from");*/
+    this.left = (this.left===this.length-1) ? 0 : this.left+1;
+    this.right = (this.right===this.length-1) ? 0 : this.right+1;
+    this.hideItems();
+}
 
-      carousel.className = "";
-      setTimeout((function() {
-        carousel.classList.add(directionClass);
-      }), 50);       
-    });
-  });
+
+let w = new displayWindow(seats);
+w.hideItems();
